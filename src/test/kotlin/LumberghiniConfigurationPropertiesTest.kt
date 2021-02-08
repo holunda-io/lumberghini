e8fde4ec3d46432e0ec85d6bad251a9a5060dc41
Jan Galinski <jan.galinski@holisticon.de>
2021-02-08 23:12:06 +0100

    version updates

--- src/test/kotlin/LumberghiniConfigurationPropertiesTest.kt
+++ src/test/kotlin/LumberghiniConfigurationPropertiesTest.kt
@@ -5,12 +5,13 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer
 import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class)
-@ContextConfiguration(initializers = [ConfigFileApplicationContextInitializer::class])
+@ContextConfiguration(initializers = [ConfigDataApplicationContextInitializer::class])
 @EnableConfigurationProperties(LumberghiniConfigurationProperties::class)
 internal class LumberghiniConfigurationPropertiesTest {
 
